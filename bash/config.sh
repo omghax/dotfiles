5a60100472d61e73d3818129346b0387bfa442da
Dray Lacy <dray@envylabs.com>
2015-11-19 11:59:53 -0500

    Enable color for `ls`

--- bash/config.sh
+++ bash/config.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 
 export EDITOR="vim"
+
+# Colors
+export LS_OPTIONS="--color=auto"
+export LSCOLORS="Bxgxfxfxcxdxdxhbadbxbx"
+export CLICOLOR="Yes"
+
+# Prompt
 export PS1="\u@\h:\w\$ "
